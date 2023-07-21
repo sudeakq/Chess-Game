d45d6090a67e8b7fca41949cdfb7b7374b095757
sudeakq <sudeakq@gmail.com>
2023-07-21 11:58:22 +0300

    created pages, packages loaded (tailwind reactdnd)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 import './App.css';
+import Board from './Board';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <>
+        <Board />
+      </>
+    </DndProvider>
   );
 }
 
