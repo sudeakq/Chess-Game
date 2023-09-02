0c8b87180af7607945a83084757b2b0dbd886cef
sudeakq <sudeakq@gmail.com>
2023-09-02 22:59:54 +0300

    update style

--- src/SquareBoard.jsx
+++ src/SquareBoard.jsx
@@ -5,7 +5,7 @@ export const SquareBoard = ({ brd }) => {
     console.log("brdd", brd);
     return (
         <div>
-            <img className='w-[40px]' src={pieceImage} alt="" />
+            <img className='w-[60px]' src={pieceImage} alt="" />
         </div>
     );
 };
