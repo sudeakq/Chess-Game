fa2df2233f21868853a6bb7320b3fb3fbd432f25
sudeakq <sudeakq@gmail.com>
2023-09-02 21:14:54 +0300

    created chess board

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 import { DndProvider } from 'react-dnd'
 import './App.css';
 import Board from './Board';
+import Game from './Game'
 
 function App() {
+  console.log(Game, "Game")
   return (
     <DndProvider backend={HTML5Backend}>
       <>
---
+++ src/Game.js
@@ -0,0 +1,6 @@
+import {
+    Chess
+} from 'chess.js'
+
+const chess = new Chess();
+export default console.log(chess.board);
