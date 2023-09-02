6e6a4a20f7af9e19cbb3d568f40df08d96ce9aad
sudeakq <sudeakq@gmail.com>
2023-09-02 22:57:41 +0300

    fix bug

--- src/SquareBoard.jsx
+++ src/SquareBoard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-
 export const SquareBoard = ({ brd }) => {
-    const pieceImage = require('../public/assets/images/${brd.type}_${brd.color}.png')
+    const pieceImage = require(`../public/assets/images/${brd.type}_${brd.color}.png`);
     console.log("brdd", brd);
     return (
         <div>
             <img className='w-[40px]' src={pieceImage} alt="" />
         </div>
-    )
-}
+    );
+};
