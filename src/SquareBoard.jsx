ef6978a42b894e70adbf8b4ef5cc1f2c90a7987f
sudeakq <sudeakq@gmail.com>
2023-09-02 23:15:20 +0300

    add useDrag function for chess pieces

--- src/SquareBoard.jsx
+++ src/SquareBoard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useDrag, DragPreviewImage } from 'react-dnd';
 
 export const SquareBoard = ({ brd }) => {
+    const [{ isDragging }, drag, preview] = useDrag({
+        type: 'chess',
+        item: {
+            id: `${brd.type}_${brd.color}`
+        },
+        collect: (monitor) => {
+            return { isDragging: ! !monitor.isDragging }
+        }
+    })
     const pieceImage = require(`../public/assets/images/${brd.type}_${brd.color}.png`);
     console.log("brdd", brd);
     return (
-        <div>
+        <div ref={drag}>
+            <DragPreviewImage src={pieceImage} connect={preview} />
             <img className='w-[60px]' src={pieceImage} alt="" />
         </div>
     );
